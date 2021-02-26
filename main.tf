59f1791853bde5bd660cd6f45bd5f06297452b6b
Renjith AC <acrenjith@gmail.com>
2021-02-26 12:08:14 +0530

    Commit1

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      //version = "=2.19"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  #   subscription_id = "*"
+  #   client_id       = "*"
+  #   client_secret   = "*"
+  #   tenant_id       = "*"
+
+}
+resource "azurerm_resource_group" "resource_gp" {
+  name     = "DemoApp1"
+  location = "eastus"
+  #   tags {
+  #     Owner = "Renji"
+  #   }
+
+}
